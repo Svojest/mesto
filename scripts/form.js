6fee02d25a73646e8f0b91a913a7bc06aed84831
Mikhail Elistratov <svojest@yandex.ru>
2022-04-11 20:36:25 +0700

    feat: добавить js popup и submit

---
+++ scripts/form.js
@@ -0,0 +1,23 @@
+
+let formElement = document.querySelector(".popup__form"); // Воспользуйтесь методом querySelector()
+
+let nameProfile = document.querySelector(".profile__title"); // Воспользуйтесь инструментом .querySelector()
+let jobProfile = document.querySelector(".profile__subtitle"); // Воспользуйтесь инструментом .querySelector()
+let nameInput = document.querySelector(".popup__heading"); // Воспользуйтесь инструментом .querySelector()
+let jobInput = document.querySelector(".popup__description"); // Воспользуйтесь инструментом .querySelector()
+let formSubmit = document.querySelector('.popup__button_save');
+// Отображение по умолчанию
+nameInput.setAttribute('value', nameProfile.textContent);
+jobInput.setAttribute('value', jobProfile.textContent);
+
+
+// Изменение
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    nameProfile.innerHTML = nameInput.value;
+    jobProfile.innerHTML = jobInput.value;
+}
+
+formElement.addEventListener("submit", formSubmitHandler);
+formSubmit.addEventListener('click', closeOverlay);
+
---
+++ scripts/popup.js
@@ -0,0 +1,23 @@
+const editProfileBtn = document.querySelector('.profile__editButton');
+const modalWindow = document.querySelector('.popup');
+const editCloseBtn = document.querySelector('.popup__button_close');
+
+// Открытие popup
+function toggleEditProfile() {
+  modalWindow.classList.toggle('popup_opened');
+}
+
+editProfileBtn.addEventListener('click', toggleEditProfile);
+editCloseBtn.addEventListener('click', toggleEditProfile);
+
+//Закрытие popup по нажатию overlay оверлею
+function closeOverlay(event) {
+    const clickOverlay = event.target === event.currentTarget;
+    if(clickOverlay) {
+        toggleEditProfile();
+    }
+}
+
+modalWindow.addEventListener('click', closeOverlay);
+
+
